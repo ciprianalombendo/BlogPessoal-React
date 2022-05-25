2e82e64533caaf8cdd64aa75a2f413de72992335
Cipriana <ciprianaluis264@gmail.com>
2022-05-25 14:05:09 -0300

    Implementando o Grid

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,1 +1,22 @@
+import { Grid, Paper } from '@material-ui/core';
+import { Grid3x3 } from '@mui/icons-material';
+import React from 'react';
 import './Home.css';
+  
+function Home(){
+    <>
+    <Grid container spacing={2}>
+        <Grid item xs={12} sm={8}>
+            <Paper style={{height: "100vh", background: "lightgrey" }}/>
+        </Grid>
+        <Grid item container direction="column" xs={12} sm={4} spacing={2}>
+            <Grid item>
+                <Paper style={{ height: "49", background: "blue" }}/>
+            </Grid>
+            <Grid item>
+                <Paper style={{height: "49vh", background: "green" }}/>
+            </Grid> 
+        </Grid>
+    </Grid>
+    </>
+}
